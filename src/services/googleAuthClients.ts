ce1fc59d8fbc8e015ddfea0d45ce2186109f4889
Marius Lungu <mariuslungu9722@gmail.com>
2023-05-16 20:39:54 +0200

    implement an app store for all google auth clients of users with google connections

---
+++ src/services/googleAuthClients.ts
@@ -0,0 +1,85 @@
+import { OAuth2Client, Credentials } from "google-auth-library";
+
+import logger from "../loaders/logger";
+import knexClient from "../loaders/knex";
+import oAuthApi from "./googleOAuth";
+
+import { Connection, IGoogleAuthClientsStore } from "../types";
+
+type TAuthClientDict = { [id: string]: OAuth2Client };
+
+const appAuthClients: TAuthClientDict = {};
+
+const handleAccessTokenRefresh = async (
+  userId: string,
+  tokens: Credentials
+) => {
+  if (userId in appAuthClients && tokens.access_token) {
+    const authClient = appAuthClients[userId];
+    try {
+      const updatedConnection = await knexClient("connections")
+        .where({ user_id: userId, provider: "GOOGLE" })
+        .update(
+          {
+            access_token: tokens.access_token,
+          },
+          ["access_token", "refresh_token"]
+        );
+
+      authClient.setCredentials({
+        access_token: (updatedConnection as any).access_token,
+        refresh_token: (updatedConnection as any).refresh_token,
+      });
+    } catch (err) {
+      logger.info("Failed to update user's access token!");
+      logger.info(err);
+    }
+  }
+};
+
+const addClient = (userId: string, authClient: OAuth2Client) => {
+  if (userId in appAuthClients) return;
+  authClient.on("tokens", (newTokens) =>
+    handleAccessTokenRefresh(userId, newTokens)
+  );
+  appAuthClients[userId] = authClient;
+};
+
+const removeClient = (userId: string) => {
+  if (!(userId in appAuthClients)) return;
+
+  delete appAuthClients[userId];
+};
+
+const getClient = (userId: string) => {
+  if (!(userId in appAuthClients)) return null;
+
+  return appAuthClients[userId];
+};
+
+const hydrateStore = async () => {
+  try {
+    const connections = await knexClient()
+      .select()
+      .from<Connection>("connections");
+    for (const connection of connections) {
+      const authClient = oAuthApi.getOAuthClient();
+      authClient.on("tokens", (newTokens) =>
+        handleAccessTokenRefresh(connection.user_id, newTokens)
+      );
+      appAuthClients[connection.user_id] = authClient;
+    }
+  } catch (err) {
+    logger.info("Failed to hydrate auth clients store!");
+    logger.info(err);
+  }
+};
+
+const googleAuthStore: IGoogleAuthClientsStore = {
+  addClient,
+  removeClient,
+  getClient,
+  hydrateStore,
+};
+
+export default googleAuthStore;
